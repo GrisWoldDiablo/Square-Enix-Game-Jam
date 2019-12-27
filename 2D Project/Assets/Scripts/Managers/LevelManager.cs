e69a3dda7015ede47bb134de630dfa49df767bb4
GrisWoldDiablo <griswoldd@hotmail.com>
2019-12-27 11:22:04 -0500

    + The Managers
    + Added the Managers for the game. They are prefabs and they do not destroy on load, So they can be drop in a scene and only the first one loaded will remain. They are also Singletons for ease of access throughout other scripts. Maybe we don't need all of those managers or we need an other one. But that's a start. Just make sure you when you add it to your scene its not a child of another object.
    + Added some folders for structure.

---
+++ 2D Project/Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    #region Singleton
+    private static LevelManager _instance = null;
+
+    public static LevelManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<LevelManager>();
+            }
+            return _instance;
+        }
+    }
+    #endregion
+
+    void Awake()
+    {
+        #region Dont Destroy On Load
+        var objects = GameObject.FindObjectsOfType(this.GetType());
+        if (objects.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+        #endregion
+    }
+}
