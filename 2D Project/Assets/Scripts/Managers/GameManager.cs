d54e7035645eafdfb55b770b764bc8982823e075
GrisWoldDiablo <griswoldd@hotmail.com>
2019-12-27 23:48:34 -0500

    + Board, Player, Tile, Dice, UI, the basics
    + We have some Tiles with different actions, Players. Dice roll and move, once you move it plays the tile actions. The snake and ladders works. Minigame just need to be added. I've made a basic board, but if can reshapped easily.
    + Had to make some placeholder sprites for tiles and player.

--- 2D Project/Assets/Scripts/Managers/GameManager.cs
+++ 2D Project/Assets/Scripts/Managers/GameManager.cs
@@ -6,6 +6,8 @@ public class GameManager : MonoBehaviour
 {
     #region Singleton
     private static GameManager _instance = null;
+    [SerializeField] private GameObject _player;
+    private int playerPosition;
 
     public static GameManager Instance
     {
@@ -30,5 +32,20 @@ public class GameManager : MonoBehaviour
         }
         DontDestroyOnLoad(this.gameObject);
         #endregion
+    }
+
+    int rolledNumber;
+    public void RollDice()
+    {
+        rolledNumber = Random.Range(1, 7);
+        UIManager.Instance.ChangeDice(rolledNumber);
+    }
+
+    public void MovePlayer()
+    {
+        PlayerManager.Instance.CurrentPlayer.MovePosition(rolledNumber);
+        rolledNumber = 0;
+        UIManager.Instance.ChangeDice(rolledNumber);
+        PlayerManager.Instance.NextPlayer();
     }
 }
---
+++ 2D Project/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int _position = 0;
+    [SerializeField] private string _name = string.Empty;
+    private Sprite _playerSprite;
+    private Color _playerColor;
+
+    public int Position { get => _position; }
+    public string Name { get => _name; set => _name = value; }
+    public Tile CurrentTile { get => BoardManager.Instance.Tiles[_position]; }
+    public Sprite PlayerSprite { get => _playerSprite; }
+    public Color PlayerColor { get => _playerColor; }
+
+    private void Awake()
+    {
+        _playerSprite = GetComponent<SpriteRenderer>().sprite;
+        _playerColor = GetComponent<SpriteRenderer>().color;
+    }
+    private void Start()
+    {
+        this.transform.position = BoardManager.Instance.Tiles[_position].transform.position;    
+    }
+
+    public void MovePosition(int value)
+    {
+        _position += value;
+        if (_position >= BoardManager.Instance.Tiles.Count)
+        {
+            _position = BoardManager.Instance.Tiles.Count - 1;
+        }
+        this.transform.position = BoardManager.Instance.Tiles[_position].transform.position;
+        PlayerManager.Instance.CurrentPlayer.CurrentTile.Action();
+    }
+
+    public void SetPosition(int value)
+    {
+        _position = value;
+        this.transform.position = BoardManager.Instance.Tiles[_position].transform.position;
+        PlayerManager.Instance.CurrentPlayer.CurrentTile.Action();
+    }
+}
