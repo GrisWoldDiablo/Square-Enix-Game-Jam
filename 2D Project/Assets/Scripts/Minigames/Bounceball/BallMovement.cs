7ad1dde30a96c5e0dbb695f1e11d3a8587db4ca7
Droodic <droodic.gamedev@gmail.com>
2019-12-30 15:57:48 -0500

    started bounceball minigame
    create bounceball minigame core
    created sprite via photoshop
    imported required sprites 
    lock movement speed of ball
    rotate slice on z axis

---
+++ 2D Project/Assets/Scripts/Minigames/Bounceball/BallMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = rb.velocity.normalized * speed;
+    }
+}
