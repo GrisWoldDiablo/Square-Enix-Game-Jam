6ccfc6d88f8564c17f8eda0db908cf39d2835993
Droodic <droodic.gamedev@gmail.com>
2019-12-29 19:02:59 -0500

    port to mobile
    +added accelerometer input for mobile @ rockfall minigame

--- 2D Project/Assets/Scripts/Minigames/Rockfall/Movement_Rockfall.cs
+++ 2D Project/Assets/Scripts/Minigames/Rockfall/Movement_Rockfall.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class Movement_Rockfall : MonoBehaviour
 {
     // Start is called before the first frame update
-    Vector2 movement;
-    [SerializeField] Rigidbody2D rb;
+    //Vector2 movement;
+    Rigidbody2D rb;
+
     [SerializeField] float speed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+        //movement.x = Input.GetAxisRaw("Horizontal");
+        //rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+
+        rb.velocity = new Vector2(Input.acceleration.x * speed, 0f);
     }
 }
