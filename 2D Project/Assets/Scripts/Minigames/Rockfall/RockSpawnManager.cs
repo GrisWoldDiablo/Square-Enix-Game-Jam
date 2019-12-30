53d47fb3c14e25fb53e9208199ad48b275695242
Droodic <droodic.gamedev@gmail.com>
2019-12-29 19:22:46 -0500

    polish
    added shadow to text 
    added ui element for start timer
    added ui element for brief instructions
    added start timer logic, will wait 5 seconds before minigame starts
    changed player sprite
    changed background color slightly

--- 2D Project/Assets/Scripts/Minigames/Rockfall/RockSpawnManager.cs
+++ 2D Project/Assets/Scripts/Minigames/Rockfall/RockSpawnManager.cs
@@ -5,27 +5,46 @@ using UnityEngine;
 
 public class RockSpawnManager : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField] float spawnRate = .5f;
     [SerializeField] Transform start;
     [SerializeField] Transform end;
     [SerializeField] GameObject spawnObject;
 
+    bool gameStarted;
     bool hasSpawned;
+    float startTimer = 5;
 
     [SerializeField] float timer = 20;
-    [SerializeField] TextMeshProUGUI timerUI;//uimanager?
+    [SerializeField] TextMeshProUGUI timerUI;
+    [SerializeField] TextMeshProUGUI instructionUI;
+    [SerializeField] TextMeshProUGUI startTimerUI;
+
+    public bool GameStarted { get => gameStarted; set => gameStarted = value; }
 
     private void Start()
     {
         timerUI.text = timer.ToString();
         hasSpawned = false;
+        Invoke("StartGame", 5f);
+    }
+
+    void StartGame()
+    {
+        instructionUI.text = "";
+        startTimerUI.text = "";
+
+        GameStarted = true;
+        if (!hasSpawned)
+        {
+            InvokeRepeating("Spawn", spawnRate, spawnRate);
+            hasSpawned = true;
+        }
     }
 
     void Spawn()
     {
         //spawnObject.transform.position = new Vector2(Random.Range(start.position.x, end.position.x), start.position.y);
-        //...Alex... Fix spawn Position and parent transform 
+
         var spawnPosition = new Vector2(Random.Range(start.position.x, end.position.x), start.position.y);
         Instantiate(spawnObject,spawnPosition,Quaternion.identity, this.transform);
         hasSpawned = false;
@@ -34,25 +53,28 @@ public class RockSpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!hasSpawned)
+        if (GameStarted)
         {
-            Invoke("Spawn", spawnRate);
-            hasSpawned = true;
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+                timerUI.text = timer.ToString("F0");
+            }
+            else if (timer <= 0)
+            {
+                //Win logic
+                timerUI.text = timer.ToString("Minigame Won");
+                GameManager.Instance.MinigameEnd(true);
+            }
         }
-
-        if(timer > 0)
+        else
         {
-            timer -= Time.deltaTime;
-            timerUI.text = timer.ToString("F0");
-        }
-        else if(timer <= 0)
-        {
-            //Win logic
-            timerUI.text = timer.ToString("Minigame Won");
-            //Time.timeScale = 0; //...Alex... Remove time scale reduction
-            GameManager.Instance.MinigameEnd(true); //...Alex... set winning status
-        }
 
-
+            if(startTimer > 0)
+            {
+                startTimer -= Time.deltaTime;
+                startTimerUI.text = startTimer.ToString("F0");
+            }
+        }
     }
 }
