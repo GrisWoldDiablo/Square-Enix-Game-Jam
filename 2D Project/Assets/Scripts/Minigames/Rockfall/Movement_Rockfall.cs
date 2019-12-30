359563d711c02699754546855236112ec1e9af75
Droodic <droodic.gamedev@gmail.com>
2019-12-29 19:22:46 -0500

    polish
    added shadow to text 
    added ui element for start timer
    added ui element for brief instructions
    added start timer logic, will wait 5 seconds before minigame starts

--- 2D Project/Assets/Scripts/Minigames/Rockfall/Movement_Rockfall.cs
+++ 2D Project/Assets/Scripts/Minigames/Rockfall/Movement_Rockfall.cs
@@ -7,12 +7,13 @@ public class Movement_Rockfall : MonoBehaviour
     // Start is called before the first frame update
     //Vector2 movement;
     Rigidbody2D rb;
-
+    RockSpawnManager miniGame;
     [SerializeField] float speed;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        miniGame = FindObjectOfType<RockSpawnManager>().GetComponent<RockSpawnManager>();
     }
 
     // Update is called once per frame
@@ -20,7 +21,10 @@ public class Movement_Rockfall : MonoBehaviour
     {
         //movement.x = Input.GetAxisRaw("Horizontal");
         //rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+        if (miniGame.GameStarted)
+        {
+            rb.velocity = new Vector2(Input.acceleration.x * speed, 0f);
+        }
 
-        rb.velocity = new Vector2(Input.acceleration.x * speed, 0f);
     }
 }
