3bb7d954ea4bf4e99b84c8c5b36f1b6a63745d8f
Droodic <droodic.gamedev@gmail.com>
2019-12-30 19:55:08 -0500

    finished core bounceball
    finished core of bounceball minigame

---
+++ 2D Project/Assets/Scripts/Managers/InstructionManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InstructionManager : MonoBehaviour
+{
+    float startTimer = 5;
+    [SerializeField] TextMeshProUGUI instructionUI;
+    [SerializeField] TextMeshProUGUI startTimerUI;
+
+    private bool gameStarted;
+
+    public bool GameStarted { get => gameStarted; set => gameStarted = value; }
+    public TextMeshProUGUI InstructionUI { get => instructionUI; set => instructionUI = value; }
+    public TextMeshProUGUI StartTimerUI { get => startTimerUI; set => startTimerUI = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Invoke("StartGame", 5f);
+    }
+
+    void StartGame()
+    {
+        instructionUI.text = "";
+        startTimerUI.text = "";
+        GameStarted = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startTimer > 0)
+        {
+            startTimer -= Time.deltaTime;
+            startTimerUI.text = startTimer.ToString("F0");
+        }
+    }
+}
--- 2D Project/Assets/Scripts/Minigames/Bounceball/BallMovement.cs
+++ 2D Project/Assets/Scripts/Minigames/Bounceball/BallMovement.cs
@@ -1,22 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class BallMovement : MonoBehaviour
 {
+    UIManager_BounceBall ui;
+    InstructionManager instructions;
+
     Rigidbody2D rb;
-    [SerializeField] float speed = 5;
+    [SerializeField] int bounceCounter = 20;
+    [SerializeField] float speed = 4;
+    int lives = 3;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        ui = FindObjectOfType<UIManager_BounceBall>().GetComponent<UIManager_BounceBall>();
         rb = GetComponent<Rigidbody2D>();
+        instructions = FindObjectOfType<InstructionManager>();
+        ui.BouncesLeftUI.text = bounceCounter.ToString();
+        ui.LivesLeftUI.text = "Lives: " + lives.ToString();
+
+        //Add random start direction
+
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Lock speed
+        
+        if (instructions.GameStarted)
+        {
+            float startDir = Random.Range(-180, 0);
+            rb.AddForce(new Vector2(startDir, startDir));
+            instructions.GameStarted = false;
+            
+        }
         rb.velocity = rb.velocity.normalized * speed;
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Slice")
+        {
+            if (bounceCounter > 0)
+            {
+                speed += 0.08f;
+                bounceCounter--;
+                ui.BouncesLeftUI.text = bounceCounter.ToString();
+
+            }
+            else
+            {
+                //Win
+                GameManager.Instance.MinigameEnd(true);
+            }
+
+        }
+        else if (lives > 0)
+        {
+            lives--;
+            ui.LivesLeftUI.text = "Lives: " + lives.ToString();
+        }
+        else
+        {
+            //Lose here
+            GameManager.Instance.MinigameEnd(false);
+        }
     }
 }
