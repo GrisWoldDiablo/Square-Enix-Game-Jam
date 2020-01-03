0d373bce749a09c2eb6a465e69c7a7c7b24ddbf7
GrisWoldDiablo <griswoldd@hotmail.com>
2020-01-03 02:59:54 -0500

    +* Sound Control
    + Added a Mute button to the Main and Start scene.
    
    + Added volume slider on the Start scene.
    * Moved the getcomponent from the start() the awake() for audiomanager.

---
+++ 2D Project/Assets/Scripts/Menu/MuteButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MuteButton : MonoBehaviour
+{
+    [SerializeField] private Image _imageMute;
+
+    private void Start()
+    {
+        _imageMute.enabled = !AudioManager.Instance.AudioListener.isActiveAndEnabled;
+    }
+
+    public void PressMute()
+    {
+        AudioManager.Instance.ToggleMute(_imageMute);
+    }
+}
