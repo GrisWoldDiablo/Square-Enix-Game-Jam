e236268430fcca8a2cb94fa38ff16ae9356f60a2
GrisWoldDiablo <griswoldd@hotmail.com>
2020-01-04 01:24:49 -0500

    +* New Minigame and more
    + New Break Away minigame, it's in 3D! you have to shoot all the Green blocks without droping the Red ones, you can only drop 2 Reds before loosing. 
    * Improved the audio manager mute button.
    * Improved the layout of the Start Scene.
    + Added Break Away to the Board game.

---
+++ 2D Project/Assets/Scripts/Minigames/Break Away/BABlock.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BABlockType
+{
+    Neutral,
+    Good,
+    Bad
+}
+public class BABlock : MonoBehaviour
+{
+    private Color _colorChoosen;
+    private Renderer _renderer;
+    private BABlockType _type;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Plane"))
+        {
+            BreakAwayGameManager.Instance.RemoveBlock(this);
+            Destroy(gameObject);
+        }
+    }
+
+    public void InitBlock(Color color, BABlockType type = BABlockType.Neutral)
+    {
+        _type = type;
+        _colorChoosen = color;
+        _renderer.material.SetColor("_Color", _colorChoosen);
+    }
+}
