b8b87ec4a1abd3bec9d6121df395d213d3d12343
Robert <roobtran@gmail.com>
2020-01-05 21:26:05 -0500

    +Lock orientation on maze mini game

---
+++ 2D Project/Assets/ScreenLock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenLock : MonoBehaviour
+{
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
