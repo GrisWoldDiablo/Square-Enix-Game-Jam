7b211b4a2c4842f171d2c86e6436fdf7ce96a3c0
GrisWoldDiablo <griswoldd@hotmail.com>
2020-01-08 23:20:35 -0500

    + Work in progess Cube Maze.
    + Working on a Cube Maze inspired by Fez.

---
+++ 2D Project/Assets/_Alex Misc/CubeSpin.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpin : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(transform.InverseTransformDirection(Vector3.up), 45 * Time.deltaTime);
+    }
+}
