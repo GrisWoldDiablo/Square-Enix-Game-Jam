12909bea6ffc01e4363cd9932c799e9afd1b263b
Robert <roobtran@gmail.com>
2020-01-15 21:15:36 -0500

    Add coregameplay
    Added battle and result from Player 1 Vs AI

---
+++ Rock-Paper-Scissors/Assets/NumberOfPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NumberOfPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadSinglePlayer()
+    {
+        SceneManager.LoadScene("SinglePlayer");
+    }
+
+    public void LoadMultiPlayer()
+    {
+        SceneManager.LoadScene("MultiPlayer");
+    }
+}
