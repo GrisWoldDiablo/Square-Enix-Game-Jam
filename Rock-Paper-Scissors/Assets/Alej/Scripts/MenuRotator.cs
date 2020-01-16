03cb415c7e6a7436318cfd812086c508162525e3
Alex Flores <SorAxel360@hotmail.com>
2020-01-16 13:50:34 -0500

    + Added Main Menu Scene
    + Added Alej folder
    + Added MAin Menu Scene
    + Added Menu Scripts

---
+++ Rock-Paper-Scissors/Assets/Alej/Scripts/MenuRotator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuRotator : MonoBehaviour
+{
+    [SerializeField] private float time = 5;
+    [SerializeField] private float speed = 5;
+
+    void Update()
+    {
+            
+    }
+
+    public void RotateLeftCaller()
+    {
+        StartCoroutine(RotateLeft());
+    }
+
+    IEnumerator RotateLeft()
+    {
+        while (time > 0)
+        {
+            transform.Rotate(0, - (Time.deltaTime * speed), 0);
+            time -= Time.deltaTime;
+
+            yield return null;
+        }
+
+        time = 2;
+    }
+
+    public void RotateRight()
+    {
+        transform.Rotate(new Vector3(0, 90, 0));
+    }
+}
